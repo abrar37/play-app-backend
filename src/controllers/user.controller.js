516e3b33c5ae52f3850f9d858feac07bd02fe81e
Abrar Ahmad <kh.abrar22@gmail.com>
2024-07-29 15:39:56 +0500

    fix: cover image issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -68,21 +68,21 @@ const registerUser = asyncHandler( async (req, res) => {
 
     // checking for image, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.fiels?.coverImage[0]?.path;
 
-    // let coverImageLocalPath;
-    // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.lenght > 0) {
-    //     coverImageLocalPath = req.files.coverImage[0].path;
-    // }
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    // console.log("coverImageLocalPath: ", coverImageLocalPath)
-    // console.log("avatarLocalPath: ", avatarLocalPath)
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
+    console.log("c-image:", req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "avatarLocalPath: Avatar file is required")
     }
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
     const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     // console.log("coverImage status:", coverImage);
 
